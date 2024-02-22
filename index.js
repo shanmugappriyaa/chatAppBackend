ee773f78c1bfa5993fadc7c0c057cdd9efd43e74
shanmugappriyaa <pspri03@gmail.com>
2024-02-22 17:53:13 +0530

    origin

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://shan-chat.netlify.app",
     credentials: true,
     optionSuccessStatus: 200,
   })
